7c863726a357a896460d6f8b5ed048a908e0af8f
Gent Rechica <gr48119@ubt-uni.net>
2022-07-24 19:30:03 +0200

    Login/Register Added + AboutUs Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 (function ($) {
 	
 	"use strict";
@@ -316,3 +317,27 @@
 
 
 })(window.jQuery);
+=======
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".change-name");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+>>>>>>> Stashed changes
